06988e70d66333f162cb1755f0bee563fdc93777
SharmagRit <sharmagrit04@gmail.com>
2022-01-29 05:41:40 +0000

    phoenix: PixelExtended bringup

---
+++ aosp_phoenix.mk
@@ -0,0 +1,28 @@
+#
+# Copyright (C) 2020 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+$(call inherit-product, device/xiaomi/phoenix/device.mk)
+
+# Inherit some common Aosp stuff.
+$(call inherit-product, vendor/aosp/config/common.mk)
+
+# Device identifier. This must come after all inclusions.
+PRODUCT_NAME := aosp_phoenix
+PRODUCT_DEVICE := phoenix
+PRODUCT_BRAND := Redmi
+PRODUCT_MODEL := Redmi K30
+PRODUCT_MANUFACTURER := Xiaomi
+
+# Aosp Stuff
+TARGET_BOOT_ANIMATION_RES := 1080
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_STOCK_ARCORE := true
+
+BUILD_FINGERPRINT := POCO/phoenixin/phoenixin:11/RKQ1.200826.002/V12.1.3.0.RGHINXM:user/release-keys
+
+PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
+
+
